a5eccc7f1bf3c63cd14c2a066e1951658b977665
davidwroche <davidwroche@yahoo.co.uk>
2017-11-06 00:39:39 +0000

    consul

--- srv/suggest-prod.js
+++ srv/suggest-prod.js
@@ -1,18 +1,28 @@
 /* Copyright (c) 2017 Richard Rodger and other contributors, MIT License */
 
-var BASES = process.env.BASES.split(',')
+//var BASES = process.env.BASES.split(',')
+var CONSUL = process.env.CONSUL_SERVICE_HOST || 'localhost'
+
 
 var Seneca = require('seneca')
 
 Seneca({tag: 'suggest'})
   .test('print')
 
+  .use('consul-registry', {
+    host: CONSUL
+  })
+
   .use('../suggest.js')
 
   .use('mesh', {
     pin: 'role:suggest',
-    bases: BASES,
+    //bases: BASES,
     host: '@eth0',
-    sneeze: {silent:false}
+    //sneeze: {silent:false}
+    discover: {
+      registry: {
+        active: true
+      }
+    }
   })
-
